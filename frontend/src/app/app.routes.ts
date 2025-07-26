5553b3013047ae3e365a4c14e30769879d50c206
Nirintsoa-3720 <razafindralambo3720@gmail.com>
2025-07-26 23:06:20 +0300

    Integration Frontend

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { Accueil } from './accueil/accueil';
+// import { Services } from './services';
+// import { Contact } from './contact';
+
+export const routes: Routes = [
+  { path: '', component: Accueil },
+//   { path: 'services', component: Services },
+//   { path: 'contact', component: Contact }
+];
